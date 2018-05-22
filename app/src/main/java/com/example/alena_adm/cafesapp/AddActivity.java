e26a98cfba62a9cfc7b04ffb0594eed3ef2c2693
AlenMuseJ <alen.muse@yandex.ru>
2018-05-22 19:38:59 +0300

    working app bar and map activity

---
+++ app/src/main/java/com/example/alena_adm/cafesapp/AddActivity.java
@@ -0,0 +1,18 @@
+package com.example.alena_adm.cafesapp;
+
+import android.app.Activity;
+import android.drm.DrmStore;
+import android.os.Bundle;
+
+/**
+ * Created by Alena_Adm on 22.05.2018.
+ */
+
+public class AddActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
---
+++ app/src/main/java/com/example/alena_adm/cafesapp/FavoriteActivity.java
@@ -0,0 +1,8 @@
+package com.example.alena_adm.cafesapp;
+
+/**
+ * Created by Alena_Adm on 22.05.2018.
+ */
+
+public class FavoriteActivity {
+}

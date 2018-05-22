3c89351042588d3f5db4f10ce0d5462b00bf8698
AlenMuseJ <alen.muse@yandex.ru>
2018-05-22 20:12:08 +0300

    favorites activity

--- app/src/main/java/com/example/alena_adm/cafesapp/FavoriteActivity.java
+++ app/src/main/java/com/example/alena_adm/cafesapp/FavoriteActivity.java
@@ -1,8 +1,17 @@
 package com.example.alena_adm.cafesapp;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Alena_Adm on 22.05.2018.
  */
 
-public class FavoriteActivity {
+public class FavoriteActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.favorite_activity);
+    }
 }
